a246849660964a27cd309f9c45e6287d61b9ce25
iyl98 <1431214761@qq.com>
2022-09-06 15:14:54 +0800

    Test git after modifying user name

--- src/test/java/com/yl/YlNetDiskMainApplicationTests.java
+++ src/test/java/com/yl/YlNetDiskMainApplicationTests.java
@@ -13,25 +13,25 @@ import java.sql.SQLException;
 @SpringBootTest
 class YlNetDiskMainApplicationTests {
 
-//    @Autowired
-//    DataSource dataSource;
-//
-//    @Autowired
-//    UserMapper userMapper;
-//
-//    //测试数据库连接
-//    @Test
-//    void contextLoads() throws SQLException{
-//        Connection connection = dataSource.getConnection();
-//        System.out.println(connection);
-//        connection.close();
-//    }
-//
-//    //测试数据库查询
-//    @Test
-//    void contextLoad_(){
-//        User user = userMapper.queryById(10000);
-//        System.out.println(user);
-//    }
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    UserMapper userMapper;
+
+    //测试数据库连接
+    @Test
+    void contextLoads() throws SQLException{
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    //测试数据库查询
+    @Test
+    void contextLoad_(){
+        User user = userMapper.queryById(10000);
+        System.out.println(user);
+    }
 
 }
